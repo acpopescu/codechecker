e937397ac9bf457de2638ac63703255144561913
Adrian Popescu <adrian-constantin.popescu@outlook.com>
2022-09-19 14:23:35 -0400

    Missed a popd :(

--- bld.bat
+++ bld.bat
@@ -1,4 +1,6 @@
 echo Building Package
 Powershell.exe -ExecutionPolicy ByPass -C ". .\Make_Win.ps1; make_dist"
 "%PYTHON%" setup.py install
-popd
+rem Clearing errorlevel as it requires build system refactor
+cmd /c "exit /b 0"
+exit 0
